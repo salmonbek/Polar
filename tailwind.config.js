a0682b38670fe4ca2444ea64d40f557d31eeef49
“salmonbek” <salmonlearner34@gmail.com>
2023-12-15 00:31:39 +0500

    start

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./pages/**/*.html"],
+  theme: {
+    extend: {},
+
+    screens: {
+      sm: "400px",
+      md: "600px",
+      lg: "900px",
+      xl: "1000px",
+      "2xl": "1160px",
+    },
+    colors: {
+      primary: "#D23166",
+      secondary: "#0F0101",
+      background: "#F8EDF0",
+    },
+    container: {
+      center: true,
+      padding: 20,
+    },
+    fontFamily: {
+      fredoka: "Fredoka",
+    },
+  },
+  plugins: [],
+};
